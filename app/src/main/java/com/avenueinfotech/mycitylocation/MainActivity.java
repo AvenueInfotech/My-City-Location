5abfd3ffd550da5ae11bf07c30060ff0b2a8ea63
Shashikant Sukenkar <sukenkar@hotmail.com>
2018-06-09 07:53:22 +0530

    Gps gor my city location
    gps activation

---
+++ app/src/main/java/com/avenueinfotech/mycitylocation/MainActivity.java
@@ -0,0 +1,513 @@
+package com.avenueinfotech.mycitylocation;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.avenueinfotech.mycitylocation.utils.GPSTracker;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks, LocationListener {
+
+
+
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
+
+    GoogleApiClient mGoogleApiClient;
+
+    private int PLACE_PICKER_REQUEST = 1;
+    private AutoCompleteAdapter mAdapter;
+
+    private TextView mTextView;
+    private AutoCompleteTextView mPredictTextView;
+
+
+    private Button btnShow;
+    private TextView addressView;
+
+    LocationManager locationManager;
+    String provider;
+    double lat = 0.0 , log=0.0;
+
+    private ProgressDialog dialog;
+    private static GPSTracker gps;
+    final int REQUEST_LOCATION = 199;
+    public final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    String[] PERMISSIONS = {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    private LocationRequest locationRequest;
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(provider,400,1, this);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-1183672799205641~3514807417");
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Google services present", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+
+        } else {
+            Toast.makeText(this, "Google services Absent", Toast.LENGTH_LONG).show();
+        }
+
+        // Load an ad into the AdMob banner view.
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
+        // Prepare the Interstitial Ad
+        interstitial = new InterstitialAd(MainActivity.this);
+// Insert the Ad Unit ID
+        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+
+        interstitial.loadAd(adRequest);
+// Prepare an Interstitial Ad Listener
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                // Call displayInterstitial() function
+                displayInterstitial();
+            }
+        });
+
+        mTextView = (TextView) findViewById(R.id.textview);
+
+        btnShow = (Button)findViewById(R.id.btnShow);
+        addressView = (TextView)findViewById(R.id.address);
+
+        mPredictTextView = (AutoCompleteTextView) findViewById(R.id.predicttextview);
+        mAdapter = new AutoCompleteAdapter(this);
+        mPredictTextView.setAdapter(mAdapter);
+
+        mPredictTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AutoCompletePlace place = (AutoCompletePlace) parent.getItemAtPosition(position);
+                findPlaceById(place.getId());
+
+
+            }
+        });
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, 0, this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+
+
+        
+//        getLocation();
+
+        gps = new GPSTracker(this);
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_ID_MULTIPLE_PERMISSIONS);
+        } else {
+
+            if (!gps.canGetLocation()) {
+                switchGPS();
+            }
+
+//            GeneralUtils.createDirectory();
+        }
+
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setCancelable(false);
+
+        itadd();
+
+        getLocation();
+    }
+
+    private void displayInterstitial() {
+
+        if (interstitial.isLoaded()) {
+            interstitial.show();
+        }
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void switchGPS() {
+        {
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(100000);
+            locationRequest.setFastestInterval(20 * 1000);
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                    .addLocationRequest(locationRequest);
+
+            // **************************
+            builder.setAlwaysShow(true); // this is the key ingredient
+            // **************************
+
+            PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                    .checkLocationSettings(mGoogleApiClient, builder.build());
+            result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                @Override
+                public void onResult(LocationSettingsResult result) {
+                    final Status status = result.getStatus();
+                    final LocationSettingsStates state = result
+                            .getLocationSettingsStates();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            // All location settings are satisfied. The client can
+                            // initialize location
+                            // requests here.
+                            break;
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be
+                            // fixed by showing the user
+                            // a dialog.
+                            try {
+                                // Show the dialog by calling
+                                // startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(MainActivity.this, REQUEST_LOCATION);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have
+                            // no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            });
+        }
+    }
+
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void getLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        final Location location = locationManager.getLastKnownLocation(provider);
+        if (location == null)
+            Log.e("ERROR", "Location is null");
+    }
+
+    private void itadd() {
+        //        callConnection();
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+                    return;
+                }
+
+                    Location myLocation = locationManager.getLastKnownLocation(provider);
+                    lat = myLocation.getLatitude();
+                    log = myLocation.getLongitude();
+
+                    new GetAddress().execute(String.format("%.4f,%.4f", lat, log));
+
+
+
+
+                }
+        });
+    }
+
+
+
+    private class GetAddress extends AsyncTask<String, Void, String> {
+//            ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        // Start long running operation in a background thread
+
+
+     @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(true);
+            dialog.show();
+        }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            double lat = Double.parseDouble(strings[0].split(",")[0]);
+            double log = Double.parseDouble(strings[0].split(",")[1]);
+            String response;
+            HttpDataHandler http = new HttpDataHandler();
+            String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=%.4f,%.4f&sensor=false", lat, log);
+            response = http.GetHTTPData(url);
+            return response;
+        } catch (Exception ex) {
+
+        }
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            String address = ((JSONArray) jsonObject.get("results")).getJSONObject(0).get("formatted_address").toString();
+            addressView.setText(address);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (dialog.isShowing())
+            dialog.dismiss();
+    }
+
+}
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mAdapter.setGoogleApiClient(null);
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    private void findPlaceById(String id) {
+        if (TextUtils.isEmpty(id) || mGoogleApiClient == null || !mGoogleApiClient.isConnected())
+            return;
+
+        Places.GeoDataApi.getPlaceById(mGoogleApiClient, id).setResultCallback(new ResultCallback<PlaceBuffer>() {
+            @Override
+            public void onResult(PlaceBuffer places) {
+                if (places.getStatus().isSuccess()) {
+                    Place place = places.get(0);
+                    displayPlace(place);
+                    mPredictTextView.setText("");
+                    mAdapter.clear();
+                }
+
+                //Release the PlaceBuffer to prevent a memory leak
+                places.release();
+            }
+        });
+    }
+
+    private void guessCurrentPlace() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback( new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult( PlaceLikelihoodBuffer likelyPlaces ) {
+
+                PlaceLikelihood placeLikelihood = likelyPlaces.get( 0 );
+                String content = "";
+                if( placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty( placeLikelihood.getPlace().getName() ) )
+                    content = "Most likely you are at: " + placeLikelihood.getPlace().getName() + "\n";
+                if( placeLikelihood != null )
+                    content += "Percent of being there: " + (int) ( placeLikelihood.getLikelihood() * 100 ) + "%";
+                mTextView.setText( content );
+
+                likelyPlaces.release();
+            }
+        });
+    }
+
+    private void displayPlacePicker() {
+        if( mGoogleApiClient == null || !mGoogleApiClient.isConnected() )
+            return;
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult( builder.build((Activity) getApplicationContext()), PLACE_PICKER_REQUEST );
+        } catch ( GooglePlayServicesRepairableException e ) {
+            Log.d( "PlacesAPI Demo", "GooglePlayServicesRepairableException thrown" );
+        } catch ( GooglePlayServicesNotAvailableException e ) {
+            Log.d( "PlacesAPI Demo", "GooglePlayServicesNotAvailableException thrown" );
+        }
+    }
+
+    protected void onActivityResult( int requestCode, int resultCode, Intent data ) {
+        if( requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK ) {
+            displayPlace( PlacePicker.getPlace( data, this ) );
+        }
+    }
+
+    private void displayPlace( Place place ) {
+        if( place == null )
+            return;
+
+        String content = "";
+        if( !TextUtils.isEmpty( place.getName() ) ) {
+            content += "Name: " + place.getName() + "\n";
+        }
+        if( !TextUtils.isEmpty( place.getAddress() ) ) {
+            content += "Address: " + place.getAddress() + "\n";
+        }
+        if( !TextUtils.isEmpty( place.getPhoneNumber() ) ) {
+            content += "Phone: " + place.getPhoneNumber();
+        }
+
+        mTextView.setText( content );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        getMenuInflater().inflate( R.menu.menu_main, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        int id = item.getItemId();
+
+        if( id == R.id.action_place_picker ) {
+            displayPlacePicker();
+            return true;
+        } else if( id == R.id.action_guess_current_place ) {
+            guessCurrentPlace();
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        log = location.getLongitude();
+
+        new GetAddress().execute(String.format("%.4f,%.4f",lat,log));
+    }
+
+
+
+}
